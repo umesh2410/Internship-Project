aed10b64ae8466a5608ea9332cced8b95855d7ba
umesh2410 <makwanau2410@gmail.com>
2025-06-07 11:57:27 +0530

    Updated all files

---
+++ New folder/Day6/BooksApi.Entities/Migrations/20250603050123_AddPermissionsToUser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BooksApi.Entities.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPermissionsToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Permission",
+                table: "Users",
+                newName: "Permissions");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Permissions",
+                table: "Users",
+                newName: "Permission");
+        }
+    }
+}
